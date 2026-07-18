6d3e1dc042ac61c8542194f618c77599fcb005b7
Mayank Ninawe <mayankninawe22@gmail.com>
2026-07-18 23:55:07 +0530

    Time: 1 ms (94.47%), Space: 43.5 MB (75.9%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        // Step 1: Check if concatenation matches
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        
+        // Step 2: Find gcd of lengths
+        int gcdLen = gcd(str1.length(), str2.length());
+        
+        // Step 3: Return substring of that gcd length
+        return str1.substring(0, gcdLen);
+    }
+    
+    // Euclidean algorithm for gcd
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
