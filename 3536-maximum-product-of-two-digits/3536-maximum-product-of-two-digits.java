8ae13a1982d39c3c8cf759e2c1d007ff6c1a2c40
Mayank Ninawe <mayankninawe22@gmail.com>
2026-07-26 01:40:53 +0530

    Time: 1 ms (100%), Space: 42.4 MB (85.44%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -2,12 +2,9 @@ class Solution {
     public int maxProduct(int n) {
         int max1 = 0, max2 = 0;
 
-        // Extract digits
         while (n > 0) {
             int digit = n % 10;
             n /= 10;
-
-            // Track top 2 largest digits
             if (digit > max1) {
                 max2 = max1;
                 max1 = digit;
